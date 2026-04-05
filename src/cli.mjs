d6825074a2612db1c080a84e114299c6fb22a7a1
McbeEringi <git@mcbeeringi.dev>
2026-04-06 02:50:59 +0900

    -

--- src/cli.mjs
+++ src/cli.mjs
@@ -6,6 +6,73 @@ import{demoji}from'./emoji.mjs';
 
 const
 msg={},
+connect=(
+	{gd,ch,g=ch.guild,observe_ch=[]},
+	{
+		conn=joinVoiceChannel({channelId:ch.id,guildId:g.id,adapterCreator:g.voiceAdapterCreator}),
+		ap=createAudioPlayer(),e=new EventTarget(),
+		url=({path,params,port=50021,base=`http://localhost:${port}`})=>Object.assign(new URL(path,base),{search:new URLSearchParams(params)}),
+		sc_q=new Set(),
+		sy=async({params,q})=>createAudioResource((await fetch(url({path:'synthesis',params}),{method:'POST',body:JSON.stringify(q)})).body),
+		pappo=w=>(
+			w=(n=>({
+				synth:Array(n)[Symbol.iterator]().map((_,i)=>(
+					i=i?i==n-1?2:1:0,
+					i==2&&sc_q.delete(w),
+					{ar:createAudioResource(Bun.file(`assets/pappo/${['start','loop','end'][i]}.opus`).stream(),{inputType:StreamType.OggOpus})}
+				)),
+				prio:1
+			}))(w+1),
+			sc_q.add(w),
+			e.dispatchEvent(new CustomEvent('add'))
+		)
+	}={}
+)=>(
+	(f=>(
+		e.addEventListener('add',async x=>f(ap.state)),
+		ap.on('stateChange',(_,x)=>f(x)),
+	))(async x=>x.status==AudioPlayerStatus.Idle&&(
+		x=[...sc_q].sort((a,b)=>b.prio-a.prio)[0]?.synth.next().value,
+		x&&ap.play(await x.ar)
+	)),
+	conn.subscribe(ap),
+	gd[g.id]={
+		conn,ap,ch,observe:new Set([ch.id,...observe_ch]),
+		disconn:_=>(conn.destroy(),delete gd[g.id],ch),
+		skip:_=>_,
+		play:async params=>((query,w,tmp)=>(
+			query=query.accent_phrases.reduce((a,x)=>(
+				a.at(-1).push(x),x.pause_mora&&a.push([]),a
+			),[[]]).map(x=>({...query,accent_phrases:x})),
+			w={
+				params,query,
+				synth:(f=>(
+					f(),
+					query[Symbol.iterator]().map(_=>(_=tmp.value,f(),tmp.done&&sc_q.delete(w),_))
+				))(
+					(i=>_=>tmp=i.next())(
+						query[Symbol.iterator]().map(q=>({ar:sy({params,q}),q}))
+					)
+				),
+				prio:1/params.text.length,
+				skip:_=>_
+			},
+			sc_q.add(w),
+			e.dispatchEvent(new CustomEvent('add')),
+			w
+		))({
+			...await(await fetch(url({path:'audio_query',params}),{method:'POST'})).json(),
+			prePhonemeLength:0,postPhonemeLength:0
+		}),
+		pappo,
+		timesignal:(x=>(
+			x=CronJob.from({
+				cronTime:x,start:true,timeZone:'Asia/Tokyo',
+				onTick:(d=new Date())=>x==gd[g.id].timesignal?pappo(d.getMinutes()?1:d.getHours()%12||12):x.stop()
+			})
+		))('0,30 * * * *')
+	}
+),
 cmds={
 	con:{
 		desc:'参加中のボイスチャンネルに接続します',
@@ -17,76 +84,7 @@ cmds={
 		)=>(
 			!g?await intr.reply('サーバでのみ有効です'):
 			!ch?await intr.reply('ボイスチャンネルに接続していないようです'):(
-				((
-					conn=joinVoiceChannel({channelId:ch.id,guildId:g.id,adapterCreator:g.voiceAdapterCreator}),
-					ap=createAudioPlayer(),e=new EventTarget(),
-					url=({path,params,port=50021,base=`http://localhost:${port}`})=>Object.assign(new URL(path,base),{search:new URLSearchParams(params)}),
-					sc_q=((s=new Set())=>Object.assign(s,{
-						at:x=>s[Symbol.iterator]().drop(x).next().value,
-						shift:_=>(_=s[Symbol.iterator]().next().value,_&&s.delete(_)&&_)
-					}))(),
-					sy=async({params,q})=>createAudioResource((await fetch(url({path:'synthesis',params}),{method:'POST',body:JSON.stringify(q)})).body),
-					pappo=w=>(
-						w=(n=>({
-							synth:Array(n)[Symbol.iterator]().map((_,i)=>(
-								i=i?i==n-1?2:1:0,
-								i==2&&sc_q.delete(w),
-								{ar:createAudioResource(Bun.file(`assets/pappo/${['start','loop','end'][i]}.opus`).stream(),{inputType:StreamType.OggOpus})}
-							)),
-							prio:1
-						}))(w+1),
-						sc_q.add(w),
-						e.dispatchEvent(new CustomEvent('add'))
-					)
-				)=>(
-					(f=>(
-						e.addEventListener('add',async x=>f(ap.state)),
-						ap.on('stateChange',(_,x)=>f(x)),
-					))(async x=>x.status==AudioPlayerStatus.Idle&&(
-						x=[...sc_q].sort((a,b)=>b.prio-a.prio)[0]?.synth.next().value,
-						x&&ap.play(await x.ar)
-					)),
-					conn.subscribe(ap),
-					gd[g.id]={
-						conn,ap,ch,observe:new Set([ch.id,intr.channelId]),
-						disconn:_=>(conn.destroy(),delete gd[g.id],ch),
-						skip:_=>_,
-						play:async params=>((query,w,tmp)=>(
-							query=query.accent_phrases.reduce((a,x)=>(
-								a.at(-1).push(x),x.pause_mora&&a.push([]),a
-							),[[]]).map(x=>({
-								...query,accent_phrases:x
-							})),
-							w={
-								params,query,
-								synth:(f=>(
-									f(),
-									query[Symbol.iterator]().map(_=>(_=tmp.value,f(),tmp.done&&sc_q.delete(w),_))
-								))(
-									(i=>_=>tmp=i.next())(
-										query[Symbol.iterator]().map(q=>({ar:sy({params,q}),q}))
-									)
-								),
-								prio:1/params.text.length,
-								skip:_=>_
-							},
-							sc_q.add(w),
-							e.dispatchEvent(new CustomEvent('add'))
-						))({
-							...await(await fetch(url({path:'audio_query',params}),{method:'POST'})).json(),
-							prePhonemeLength:0,postPhonemeLength:0
-						}),
-						pappo,
-						timesignal:(x=>(
-							x=CronJob.from({
-								cronTime:x,
-								onTick:(d=new Date())=>x==gd[g.id].timesignal?pappo(d.getMinutes()?1:d.getHours()%12||12):x.stop(),
-								start:true,
-								timeZone:'Asia/Tokyo'
-							})
-						))('0,30 * * * *')
-					}
-				))().play((i=>({
+				connect({gd,ch,g,observe_ch:[intr.channelId]}).play((i=>({
 					speaker:i,
 					text:i&1?'接続したのだ':'接続しました'
 				}))(Math.random()*4|0)),
@@ -191,11 +189,15 @@ main=({
 		),{}))
 	})),
 	cli.on(Events.VoiceStateUpdate,async(a,b)=>b.member.user.bot||(
-		(!a.channel&&b.channel&&b.channel.id==gd[a.guild.id]?.ch.id)&&await gd[a.guild.id]?.play({
-			// speaker:0,
-			speaker:BigInt(b.member.id)%4n,
-			text:`${b.member.user.displayName} さんが入室し${b.member.id%2?'たのだ':'ました'}`
-		}),
+		(!a.channel&&b.channel&&b.channel.id==gd[a.guild.id]?.ch.id)&&(
+			await gd[a.guild.id]?.play({
+				// speaker:0,
+				speaker:BigInt(b.member.id)%4n,
+				text:`${b.member.user.displayName} さんが入室し${b.member.id%2?'たのだ':'ました'}`
+			})||(
+				b.selfMute&&b.channel.members.filter(x=>!x.user.bot).size==2&&connect({gd,ch:b.channel})
+			)
+		),
 		(a.channel&&!b.channel&&a.channel.id==gd[a.guild.id]?.ch.id)&&(
 			a.channel.members.filter(x=>!x.user.bot).size?await gd[a.guild.id]?.play({
 				// speaker:0,
@@ -205,10 +207,7 @@ main=({
 		)
 	)),
 	cli.once(Events.ClientReady,async cli=>(
-		// await cli.application.commands.set(
-		// 	Object.entries(cmds).map(([k,v])=>({name:k,description:v.desc}))
-		// )
-		await new REST({version:'10'}).setToken(cli.token).put(
+		await new REST().setToken(cli.token).put(
 			Routes.applicationCommands(cli.user.id),
 			{body:Object.entries(cmds).map(([k,v])=>(v.build??(_=>_))(new SlashCommandBuilder().setName(k).setDescription(v.desc)).toJSON())}
 		),
