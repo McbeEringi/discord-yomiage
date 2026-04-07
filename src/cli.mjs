7efaab6b05a57a38bd92cd2d1c7bf978ad2549fa
McbeEringi <git@mcbeeringi.dev>
2026-04-07 21:43:25 +0900

    -

--- src/cli.mjs
+++ src/cli.mjs
@@ -130,7 +130,7 @@ connect=(
 		timesignal:(x=>(
 			x=CronJob.from({
 				cronTime:x,start:true,timeZone:'Asia/Tokyo',
-				onTick:(d=new Date())=>x==gd[g.id].timesignal?pappo(d.getMinutes()?1:d.getHours()%12||12):x.stop()
+				onTick:(d=new Date())=>x==gd[g.id]?.timesignal?pappo(d.getMinutes()?1:d.getHours()%12||12):x.stop()
 			})
 		))('0,30 * * * *')
 	}
