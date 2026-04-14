c18cd81e680d9641b562eda333809d8cd71763b5
McbeEringi <git@mcbeeringi.dev>
2026-04-15 04:03:08 +0900

    -

--- src/cli.mjs
+++ src/cli.mjs
@@ -117,7 +117,7 @@ connect=(
 )=>(
 	(f=>(
 		e.addEventListener('add',x=>f(ap.state)),
-		ap.on('stateChange',(_,x)=>f(x)),
+		ap.on('stateChange',(_,x)=>f(x))
 	))(async x=>x.status==AudioPlayerStatus.Idle&&(
 		x=sc_q[Symbol.iterator]().reduce((a,b)=>a?a.prio<b.prio?b:a:b,null),
 		ap.cur=x,
